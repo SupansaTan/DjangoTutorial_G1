e3fef92d8eb2122dc09286d56b1e1bf0efc99127
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2021-02-15 15:29:36 +0700

    Add class Vote in models

--- polls/models.py
+++ polls/models.py
@@ -20,7 +20,10 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    vote_time = models.DateTimeField('vote time')
 
     def __str__(self):
         return self.choice_text
+
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    time = models.DateTimeField('time', blank=True)
