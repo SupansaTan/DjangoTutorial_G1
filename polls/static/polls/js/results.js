7a818a2e2667c8e3b14e3d05dc63016e00976420
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2021-02-21 03:47:02 +0700

    Can click button to sorting last vote time in results template

--- polls/static/polls/js/results.js
+++ polls/static/polls/js/results.js
@@ -1,4 +1,5 @@
-let reverse = false; // if reverse will be sort ascending
+let reverseVotes = false; // if reverse will be sort ascending
+let reverseLastVoteTime = true; // if reverse will be sort date time descending
 
 function sortVoteScore()
 {
@@ -30,22 +31,73 @@ function sortVoteScore()
     })
 
     /* sort ascending */
-    if(reverse)
+    if(reverseVotes)
     {
         choice_wrap_list.reverse();
     }
 
-
     /* change sort format and change the arrow in button */
-    if(reverse){
-        reverse = false;
-        $('#arrow').removeClass('arrowup');
-        $('#arrow').addClass('arrowdown');
+    if(reverseVotes){
+        reverseVotes = false; // next times will be sort descending
+        $('#arrow_votes').removeClass('arrow up');
+        $('#arrow_votes').addClass('arrow down');
     }
     else{
-        reverse = true;
-        $('#arrow').removeClass('arrowdown');
-        $('#arrow').addClass('arrowup');
+        reverseVotes = true; // next times will be sort ascending
+        $('#arrow_votes').removeClass('arrow down');
+        $('#arrow_votes').addClass('arrow up');
+    }
+
+    /* clear old choices and replace with all choices were sorted on template */
+    choice_div.innerHTML = ""
+    choice_wrap_list.forEach((choice)=>{
+        document.getElementsByClassName("choice")[0].innerHTML += choice.html.outerHTML
+    })
+}
+
+function sortLastVoteTime()
+{
+    /* get container of all choices */
+    let choice_div = document.getElementsByClassName("choice")[0]
+
+    /* get choice wrapper */
+    let choice_wrappers = choice_div.getElementsByClassName("choice-wrapper")
+    let choice_wrap_list = []
+
+    /* get time for each choice to add object in list */
+    for (let i=0;i<choice_wrappers.length;i++)
+    {
+        choice_wrap = choice_wrappers[i]
+        time = choice_wrap.getElementsByClassName("lasttime-vote")[0].getElementsByTagName("p")[0].innerHTML.split(" ") // get date-time in tag <p> 
+        date = time[4].split("/")
+        datetime_new_format = date[2].substring(0,4) + "/" + date[1] + "/" + date[0] + " " + time[5]
+        choice = {"time":datetime_new_format, "html":choice_wrap}
+        choice_wrap_list.push(choice)
+    }
+
+    /* sort ascending */
+    choice_wrap_list.sort(function(a,b){
+        let AdateTime = new Date(a.time);
+        let BdateTime = new Date(b.time);
+        return new Date(AdateTime) - new Date(BdateTime);
+    })
+
+    /* sort descending */
+    if(reverseLastVoteTime)
+    {
+        choice_wrap_list.reverse();
+    }
+
+    /* change sort format and change the arrow in button */
+    if(reverseLastVoteTime){
+        reverseLastVoteTime = false; // next times will be sort ascending
+        $('#arrow_voteTime').removeClass('arrow down');
+        $('#arrow_voteTime').addClass('arrow up');
+    }
+    else{
+        reverseLastVoteTime = true; // next times will be sort descending
+        $('#arrow_voteTime').removeClass('arrow up');
+        $('#arrow_voteTime').addClass('arrow down');
     }
 
     /* clear old choices and replace with all choices were sorted on template */
