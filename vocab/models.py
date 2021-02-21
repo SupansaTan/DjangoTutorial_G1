6a68930fda330286d3c8a4bbc269c5f5120b86e9
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2021-02-21 18:00:43 +0700

    creating models

--- vocab/models.py
+++ vocab/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Vocabs(models.Model):
+    vocabs_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.vocabs_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+class Means(models.Model):
+    vocabs = models.ForeignKey(Vocabs, on_delete=models.CASCADE)
+    means_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.means_text
