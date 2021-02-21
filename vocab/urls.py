824fe8610bc071e423e29b57ccf49af26d89c027
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2021-02-22 02:39:28 +0700

    table vocabulary and form add vocab

--- vocab/urls.py
+++ vocab/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'vocab'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<form/', views.detail, name='detail'),
     path('add/', views.addvocab, name='add'),
 ]
--- vocab/views.py
+++ vocab/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import get_object_or_404, render
-
+from django.contrib.auth.models import User
 from .models import Vocab
 
 
 def index(request):
+    data = Vocab.objects.all()
+    return render(request, 'vocab/index.html', {'vocabulary':data})
+
+def detail(request):
+    name = request.POST['name']
+    descripion = request.POST['descripion']
+    user = User.objects.create_vocabs(
+        vocabs_text = name,
+        means = descripion
+    )
+    user.save()
+
     data = Vocab.objects.all()
     return render(request, 'vocab/index.html', {'vocabulary':data})
 
