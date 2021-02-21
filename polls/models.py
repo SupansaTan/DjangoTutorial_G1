375ae55e5938c446420a25eaf95a9c94f050dc04
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2021-02-21 14:34:43 +0700

    Can sort question list by sum votes in index template

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,6 @@
 import datetime
 
 from django.db import models
-from django.db.models import Sum
 from django.utils import timezone
 
 tz = timezone.get_default_timezone()
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.utils.datastructures import MultiValueDictKeyError
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
@@ -20,7 +21,6 @@ class IndexView(generic.ListView):
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
 
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
@@ -34,6 +34,35 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+def index(request):
+    button = request.GET.get('sort', False)
+    
+    if button == 'votes':
+        sum_votes = []
+        question_id_sorted = []
+        questions = Question.objects.all()
+
+        for question in questions:
+            question_obj = {'id':question.id, 'votes':question.get_sum_score()}
+            sum_votes.append(question_obj)
+        sum_votes =sorted(sum_votes, key=lambda question: question['votes'], reverse=True)
+
+        for question in sum_votes:
+            question_id_sorted.append(question['id'])
+
+        votes_sort = questions.filter(id__in = question_id_sorted)
+        sorted(votes_sort, key=lambda i: question_id_sorted.index(i.pk))
+
+        return render(request, 'polls/index.html', {
+            'latest_question_list': votes_sort
+        })
+
+    return render(request, 'polls/index.html', {
+            'latest_question_list': Question.objects.filter(
+                pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
+    })
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
