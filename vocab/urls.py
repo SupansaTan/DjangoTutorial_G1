73b32a47f55a2900d23421a48b402fbddca29554
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2021-02-25 03:31:01 +0700

    can delete vocab

--- vocab/urls.py
+++ vocab/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:vocab_id>/', views.detail, name='detail'),
     path('add/', views.addVocab, name='add'),
+    path('delete/<int:vocab_id>/', views.deleteVocab, name='delete'),
     path('search/', views.search, name='search')
 ]
