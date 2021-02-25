cdbd53a5d1abc7754cbcb8cf20d96f404d0ccba1
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2021-02-25 14:35:03 +0700

    Add comments

--- vocab/views.py
+++ vocab/views.py
@@ -4,12 +4,12 @@ from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.urls import reverse
 from .models import Vocab, Mean
 
-def index(request):
-    latest_vocab_list = Vocab.objects.order_by('-pub_date')[:4]
+def index(request): # show recently added vocabulary
+    latest_vocab_list = Vocab.objects.order_by('-pub_date')[:4] # 4 recently added words
     context = {'latest_vocab_list': latest_vocab_list}
     return render(request, 'vocab/index.html', context)
 
-def search(request):
+def search(request): # when submitted search button 
     search_text = request.GET.get('search_box')  # set search_text value from tag input
     vocab_text = Vocab.objects.filter(Q(vocab_text__icontains=search_text.lower())) # get vocabs from database
 
@@ -20,12 +20,14 @@ def search(request):
             'seach_text': search_text
         })
     else:
+        # not found vocab
         return render(request, 'vocab/index.html', {
         'error_message': f"'{search_text}' not found.",
-        'latest_vocab_list': Vocab.objects.order_by('-pub_date')[:4]
+        'latest_vocab_list': Vocab.objects.order_by('-pub_date')[:4] # 4 recently added words
     })
 
-def detail(request, vocab_id):
+def detail(request, vocab_id): # show detail about that vocab
+    # get detail
     word = Vocab.objects.get(pk=vocab_id)
     word_type = word.get_type()
     word_mean = word.get_meaning()
@@ -34,10 +36,10 @@ def detail(request, vocab_id):
     return render(request, 'vocab/detail.html', {
         'vocabulary': vocab_detail,
         'vocab': word
-    })  
+    })
 
 def addVocab(request):
-    if request.method == 'GET':
+    if request.method == 'GET': # show form input
         return render(request, 'vocab/add.html')
 
     elif request.method == 'POST': # if submitted form
@@ -46,11 +48,13 @@ def addVocab(request):
         meaning = request.POST.get('meaning').strip()
 
         if not Vocab.objects.filter(Q(vocab_text=word)).exists(): # if it is new word
-            # create word and meaning
+            # create word and meaning & save to database
             create_word = Vocab(vocab_text=word)
             create_meaning = Mean(vocab=create_word, type=word_type, means_text=meaning)
             create_word.save()
             create_meaning.save()
+
+            # show alert successful
             return render(request,'vocab/add.html',{
                 'success': f"Successful ! '{word}' has been added."
             })
@@ -59,16 +63,20 @@ def addVocab(request):
             existsWord = Vocab.objects.get(vocab_text=word) # get word
 
             if not Mean.objects.filter(Q(vocab=existsWord) & Q(means_text=meaning)).exists(): # if not have meaning of this vocab
+                # create new meaning & save to database
                 create_meaning = Mean(vocab=existsWord, type=word_type, means_text=meaning)
                 create_meaning.save()
+
+                # show alert successful
                 return render(request,'vocab/add.html',{
                     'success': f"Successful! '{word}' has been added."
                 })
         
+        # show alert danger
         return render(request,'vocab/add.html',{
                     'failed': f"Oops ! '{word}' already exists"
                 })
                 
 def deleteVocab(request, vocab_id):
-    Vocab.objects.get(id=vocab_id).delete()
-    return HttpResponseRedirect('/vocab/')
+    Vocab.objects.get(id=vocab_id).delete() # delete vocab
+    return HttpResponseRedirect('/vocab/') # redirect to index page
