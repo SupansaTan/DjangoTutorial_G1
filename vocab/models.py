bee2425a4f66c52445bae44bd93ddb8972813f40
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2021-03-03 17:50:08 +0700

    Vocabulary can have many meanings and schemes.

--- vocab/models.py
+++ vocab/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.db.models import Prefetch
 
 class Vocab(models.Model):
     vocab_text = models.CharField(max_length=200)
@@ -13,15 +14,20 @@ class Vocab(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def get_meaning(self):
-        return self.meaning.get(vocab=self.id).means_text
+        meaning_list = list(self.meaning.filter(vocab=self.id).distinct())
+        return meaning_list
 
     def get_type(self):
-        return self.meaning.get(vocab=self.id).type
+        type_list = list(self.meaning.filter(vocab=self.id).distinct())
+        return type_list 
 
 class Mean(models.Model):
-    vocab = models.ForeignKey(Vocab, related_name='meaning', on_delete=models.CASCADE)
+    vocab = models.ForeignKey(
+        Vocab, related_name='meaning', on_delete=models.CASCADE)
     type = models.CharField(max_length=200)
     means_text = models.CharField(max_length=400)
 
     def __str__(self):
         return self.means_text
+    
+    
